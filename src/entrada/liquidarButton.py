6aefd86e60c4d6a195a6e226355ceaa68d17cc55
Ezequiel Rodriguez <ezequielmatiasr43@gmail.com>
2025-02-02 11:28:09 -0300

    Updated: liquidarButton and its VBA Macro

--- src/entrada/liquidarButton.py
+++ src/entrada/liquidarButton.py
@@ -7,33 +7,36 @@ def liquidar_button(button):
     # Get the active workbook and sheet
     wb = xw.Book.caller()
     sheet = wb.sheets.active
-    # Get the source sheet (ENTRADA) where the values will be retrieve and the target sheet (FACTURACION) where the values will be pasted
+    # Get the source sheet (ENTRADA) where the values will be retrieve and the target sheet (LIQUIDACIONES) where the values will be pasted
     entrada_sheet = wb.sheets["ENTRADA"]
-    facturacion_sheet = wb.sheets["FACTURACION"]
+    liquidaciones_sheet = wb.sheets["LIQUIDACIONES"]
 
     # Get row number of the button pressed
     active_button = sheet.shapes[button]
-    button_row = active_button.top_left_cell.row
+    button_row = active_button.api.TopLeftCell.Row
 
     # Retrieve values from ENTRADA sheet
     date = entrada_sheet.range(f"B{button_row}").value
-    provider = entrada_sheet.sheet.range(f"C{button_row}").value
+    provider = entrada_sheet.range(f"C{button_row}").value
     article = entrada_sheet.range(f"D{button_row}").value
     quantity = entrada_sheet.range(f"E{button_row}").value
     price = entrada_sheet.range(f"F{button_row}").value
 
-    # Paste values to FACTURACION sheet
-    if not facturacion_sheet.range("B2").value:  # Pastes if date field is empty
-        facturacion_sheet.range("B2").value = date
+    liquidaciones_sheet.api.Rows(13).Insert(Shift=-4121)  # Inserts new empty row
+    # Paste values to LIQUIDACIONES sheet
+    liquidaciones_sheet.range("C5:D5").value = provider
+    liquidaciones_sheet.range("B13").value = date
+    liquidaciones_sheet.range("C13").value = quantity
+    liquidaciones_sheet.range("D13").value = article
+    liquidaciones_sheet.range("E13").value = price
 
-    facturacion_sheet.range("C5:D5").value = provider
-    if textUp:  # Makes a row break if the text above is not empty
-        # Salto de linea
-        facturacion_sheet.range("").value = article
-        facturacion_sheet.range("").value = quantity
-        facturacion_sheet.range("").value = price
+    # Change the font color in ENTRADA to green
+    entrada_sheet.range(f"B{button_row}:G{button_row}").color = (10, 150, 10) # Green
 
     # Displays a message box
     root = tk.Tk()
     root.withdraw()  # Hide the root window
-    messagebox.showinfo(f"Fila {button_row}: Añadida satisfactoriamente", f"La entrada en fila N° {button_row} fue añadida satisfactoriamente a la liquidación")  # Info message box
+    messagebox.showinfo(
+        f"Fila {button_row}: Añadida satisfactoriamente",
+        f"La entrada en fila N° {button_row} fue añadida satisfactoriamente a la liquidación",
+    )  # Info message box
